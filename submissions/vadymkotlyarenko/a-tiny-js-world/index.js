86accc3c04a4a002fea7e322d64ef01f70d03285
Vadym Kotlyarenko <vadymkotyarenko@gmail.com>
2019-01-16 23:06:56 +0200

    change after review

--- submissions/vadymkotlyarenko/a-tiny-js-world/index.js
+++ submissions/vadymkotlyarenko/a-tiny-js-world/index.js
@@ -1,39 +1,72 @@
-function Inhabitant(species, name, gender, legs, hands, saying, ...friends) {
-    this.species = species;
-    this.name = name;
-    this.gender = gender;
-    this.legs = legs;
-    this.hands = hands;
-    this.saying = saying;
-    this.friends = friends;
+class Inhabitant {
+    constructor(species, name, gender, legs, saying, ...friends) {
+        this.species = species;
+        this.name = name;
+        this.gender = gender;
+        this.legs = legs;
+        this.friends = friends;        
+        this.saying = saying;
+    }
+    toString() {
+        return [
+            `Species => ${this.species}`,
+            `Name => ${this.name}`,
+            `Gender => ${this.gender}`,
+            `Legs => ${this.legs}`,
+            `Friends => ${this.friends.length > 0 ? this.getFriendsName(this.friends) : "No one"}`,
+        ].join("; ")
+    }
+    getFriendsName(friends) {
+        let names = "";
+        friends.forEach(friend => {
+            names += friend.name + ", ";
+        });
+        return names;
+    }
 };
 
-Inhabitant.prototype.getTextForPrint = function() {
-    return [
-        `Species => ${this.species}`,
-        `Name => ${this.name}`,
-        `Gender => ${this.gender}`,
-        `Legs => ${this.legs}`,
-        `Hands => ${this.hands}`,
-        `Saying => ${typeof(this.saying) === "object" ? this.saying.saying : this.saying}`,
-        `Friends => ${this.friends.length > 0 ? this.getFriendsName() : "No one"}`
-    ].join("; ")
-};
-Inhabitant.prototype.getFriendsName = function() {
-    let names = "";
-    this.friends.forEach(friend => {
-        names += friend.name + ", ";
-    });
-    return names;
-};
+class Human extends Inhabitant {
+    constructor(name, gender, legs, hands, saying, ...friends) {
+        super("human", name, gender, legs, hands, saying, ...friends)
+        this.hands = hands;
+    }
+    toString() {
+        return [
+            super.toString(),
+            `Saying => ${this.saying}`
+        ].join("; ")
+    }
+}
 
-const dog = new Inhabitant("dog", "Buddy", "male", 4, 0, "woof-woof!");
-print(dog.getTextForPrint());
-const cat = new Inhabitant("cat", "Garfield", "male", 4, 0, "meu", dog);
-print(cat.getTextForPrint());
-const man = new Inhabitant("human", "Jonathan", "male", 2, 2, "Hello!", dog, cat);
-print(man.getTextForPrint());
-const woman = new Inhabitant("human", "Megan", "female", 2, 2, "Hi!");
-print(woman.getTextForPrint());
-const catWomen = new Inhabitant("cat", "Jane", "female", 4, 0, cat);
-print(catWomen.getTextForPrint());
+class Dog extends Inhabitant {
+    constructor(name, gender, legs, saying, ...friends) {
+        super("dog", name, gender, legs, saying, ...friends)
+    }
+    toString() {
+        return [
+            super.toString(),
+            `Saying => ${this.saying}`
+        ].join("; ")
+    }
+}
+
+class Cat extends Inhabitant {
+    constructor(name, gender, legs, saying, ...friends) {
+        super("cat", name, gender, legs, saying, ...friends)
+    }
+    toString() {
+        return [
+            super.toString(),
+            `Saying => ${typeof(this.saying) === "object" ? this.saying.saying : this.saying}`
+        ].join("; ")
+    }
+}
+
+const dog = new Dog("Buddy", "male", 4, "woof-woof!");
+const cat = new Cat("cat", "Garfield", "male", 4, "meu", dog);
+const man = new Human("Jonathan", "male", 2, 2, "Hello!", dog, cat);
+const woman = new Human("Megan", "female", 2, 2, "Hi!");
+const catWomen = new Cat("Jane", "female", 4, cat);
+[dog, cat, man, woman, catWomen].forEach(item => 
+    print(item.toString())
+);
